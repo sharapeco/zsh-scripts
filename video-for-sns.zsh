551d1d13837866300b8a03807f02cc5db52de1ab
Shinichiro Yabu <scin.ichi@gmail.com>
2023-03-07 20:25:04 +0900

    サイズを追加しパラメータを調整

--- video-for-sns.zsh
+++ video-for-sns.zsh
@@ -13,10 +13,11 @@ function video-for-twitter() {
 	fi
 	ffmpeg \
 		-i $1 \
-		-c:v libx264 \
-		-c:a aac \
-		-vf fps=30 \
-		-vf format=yuv420p \
+		-codec:v h264_videotoolbox \
+		-minrate:v 3400k \
+		-codec:a aac \
+		-filter:v fps=30 \
+		-filter:v format=yuv420p \
 		"${1:r}-twitter.mp4"
 }
 
@@ -32,18 +33,69 @@ function video-for-twitter-no-audio() {
 	fi
 	ffmpeg \
 		-i $1 \
-		-c:v libx264 \
+		-codec:v h264_videotoolbox \
+		-minrate:v 3400k \
 		-an \
-		-vf fps=30 \
-		-vf format=yuv420p \
+		-filter:v fps=30 \
+		-filter:v format=yuv420p \
 		"${1:r}-twitter-no-audio.mp4"
 }
 
-# 動画を小さいサイズ (30fps, 720p) に変換する
-function video-size-reduce() {
+# 動画を小さいサイズ (30fps, 1080p) に変換する
+function video-size-1080p() {
 	help() {
-		echo 'Usage: video-size-reduce input'
-		echo '  foo.mp4 -> foo-reduced.mp4'
+		echo 'Usage: video-size-1080p input'
+		echo '  foo.mp4 -> foo-1080p.mp4'
+	}
+	if [ "$1" = "" ]; then
+		help
+		return
+	fi
+
+	local filter="format=yuv422p"
+
+	local fps=`video-get-fps "${1}"`
+	case ${fps} in
+		"50" )
+			filter="${filter}, fps=25"
+		;;
+		"59.94" )
+			filter="${filter}, fps=29.97"
+		;;
+		"60" )
+			filter="${filter}, fps=30"
+		;;
+		"100" )
+			filter="${filter}, fps=25"
+		;;
+		"119.88" )
+			filter="${filter}, fps=29.97"
+		;;
+		"120" )
+			filter="${filter}, fps=30"
+		;;
+	esac
+
+	ffmpeg \
+		-i "${1}" \
+		-codec:v h264_videotoolbox \
+		-b:v 6M \
+		-maxrate:v 10M \
+		-minrate:v 3M \
+		-filter:v ${filter} \
+		-s 1920x1080 \
+		"${1:r}-1080p.mp4"
+
+	local original_size=`wc -c < "${1}"`
+	local reduced_size=`wc -c < "${1:r}-1080p.mp4"`
+	printf "ORIGINAL: %'d bytes ===> REDUCED: %'d bytes\n" ${original_size} ${reduced_size}
+}
+
+# 動画を小さいサイズ (30fps, 720p) に変換する
+function video-size-720p() {
+	help() {
+		echo 'Usage: video-size-720p input'
+		echo '  foo.mp4 -> foo-720p.mp4'
 	}
 	if [ "$1" = "" ]; then
 		help
@@ -76,12 +128,14 @@ function video-size-reduce() {
 
 	ffmpeg \
 		-i "${1}" \
+		-codec:v h264_videotoolbox \
+		-minrate:v 3400k \
 		-filter:v ${filter} \
 		-s 1280x720 \
-		"${1:r}-reduced.mp4"
+		"${1:r}-720p.mp4"
 
 	local original_size=`wc -c < "${1}"`
-	local reduced_size=`wc -c < "${1:r}-reduced.mp4"`
+	local reduced_size=`wc -c < "${1:r}-720p.mp4"`
 	printf "ORIGINAL: %'d bytes ===> REDUCED: %'d bytes\n" ${original_size} ${reduced_size}
 }
 
