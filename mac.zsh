8a734d8bffb975a23bba3023ef9cd9d8890fcb34
Shinichiro Yabu <scin.ichi@gmail.com>
2026-01-20 20:35:36 +0900

    ✨ Time Machine バックアップから特定のパスを除外する関数を追加

--- mac.zsh
+++ mac.zsh
@@ -19,3 +19,36 @@ alias cot='open -a CotEditor'
 # % command arg | C
 # ...Shift キーを押したまま打てるのがポイント
 alias C='pbcopy'
+
+# Time Machine バックアップから除外
+function tmexclude() {
+	local rules=('^node_modules$' '^__pycache__$')
+
+	if [[ $1 == "sim" ]]; then
+		echo "🕔 Time Machine バックアップから次のパスを除外します"
+		for rule in $rules; do
+			fd --hidden --no-ignore --type d --prune "$rule"
+		done
+		return
+	fi
+
+	if [[ $1 == "run" ]]; then
+		for rule in $rules; do
+			fd --hidden --no-ignore --type d --prune "$rule" -x tmutil addexclusion {}
+		done
+		echo "🕔 Time Machine バックアップの除外設定を追加しました"
+		return
+	fi
+
+	echo "🕔 tmexclude – Time Machine バックアップから特定のパスを除外"
+	echo "Usage: tmexclude <sim|run>"
+	echo ""
+	echo " sim – 除外されるパスをシミュレーション表示"
+	echo " run – 除外を実行"
+	echo ""
+	echo "カレントディレクトリ以下の次のパスを除外します:"
+	for rule in $rules; do
+		echo "  - $rule"
+	done
+	return 1
+}
